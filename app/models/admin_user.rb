2aa43580a1bf1ae471f9b79b73c5324ccb9fc3ed
andrea longhi <andrea@spaghetticode.it>
2015-10-05 12:03:22 +0200

    add model code

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -7,4 +7,39 @@ class AdminUser < ActiveRecord::Base
          :two_factor_authenticatable,
          :otp_secret_encryption_key => ENV['TWO_FACTOR_KEY']
 
+
+  def activate_two_factor params
+    otp_params = { otp_secret: unconfirmed_otp_secret }
+    if !valid_password?(params[:password])
+      errors.add :password, :invalid
+      false
+    elsif !validate_and_consume_otp!(params[:otp_attempt], otp_params)
+      errors.add :otp_attempt, :invalid
+      false
+    else
+      activate_two_factor
+    end
+  end
+
+  def deactivate_two_factor params
+    if !valid_password?(params[:password])
+      errors.add :password, :invalid
+      false
+    else
+      self.otp_required_for_login = false
+      self.otp_secret = nil
+      save
+    end
+  end
+
+  private
+
+  def activate_two_factor
+    self.otp_required_for_login = true
+    self.otp_secret = current_admin_user.unconfirmed_otp_secret
+    self.unconfirmed_otp_secret = nil
+    save
+  end
+
+
 end
